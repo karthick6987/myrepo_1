72ecbd8db56fee2096bcf98109beb7e5b0daa44a
karthick6987 <142716948+karthick6987@users.noreply.github.com>
2025-09-23 21:55:40 +0530

    Create main.tf

---
+++ Module/VM/main.tf
@@ -0,0 +1,36 @@
+resource "azurerm_network_interface" "vm_nic" {
+  name                = "${var.name}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "vm" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  size                = var.size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.vm_nic.id
+  ]
+
+  os_disk {
+    name                 = var.os_disk
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = var.image_os
+    version   = "latest"
+  }
+}
