47b4885289c3f43f6b8827a6c0b43650f5cb718f
karthick6987 <142716948+karthick6987@users.noreply.github.com>
2025-09-24 10:17:11 +0530

    Update output.tf

--- Module/VM/output.tf
+++ Module/VM/output.tf
@@ -7,3 +7,8 @@ output "nic_id" {
   description = "ID of the Network Interface"
   value       = azurerm_network_interface.vm_nic.id
 }
+
+output "public_ip" {
+  description = "output of the public IP"
+  value       = azurerm_windows_virtual_machine.vm.id
+}
